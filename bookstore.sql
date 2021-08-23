fb578d84d2e306caef27a9676d3b40b6fe90f972
mildred.vittor@amalitech.org <mildred.vittor@amalitech.org>
2021-08-23 13:35:08 +0000

    new

--- bookstore.sql
+++ bookstore.sql
@@ -2,4 +2,41 @@ select * from customers limit 10;
 select * from oders limit 10;
 select * from books limit 10;
 
-select pg_indexes;
+select pg_indexes
+WHERE tablename = 'customers';
+SELECT *
+FROM pg_Indexes
+WHERE tablename = 'orders';
+SELECT *
+FROM pg_Indexes
+WHERE tablename = 'books';
+
+CREATE INDEX customer_id_idx ON orders (customer_id);
+
+EXPLAIN ANALYZE SELECT original_language, title, sales_in_millions FROM books 
+WHERE original_language = 'French';
+
+SELECT pg_size_pretty (pg_total_relation_size('books'));
+
+CREATE INDEX books_original_language_title_sales_in_millions_idx 
+ON books (original_language, title, sales_in_millions);
+
+EXPLAIN ANALYZE SELECT * FROM books;
+
+SELECT pg_size_pretty (pg_total_relation_size('books'));
+
+SELECT NOW();
+ 
+\COPY orders FROM 'orders_add.txt' DELIMITER ',' CSV HEADER;
+ 
+SELECT NOW();
+
+DROP INDEX IF EXISTS books_original_language_title_sales_in_millions_idx;
+DROP INDEX IF EXISTS 
+customer_id_idx;
+
+SELECT NOW();
+ 
+\COPY orders FROM 'orders_add.txt' DELIMITER ',' CSV HEADER;
+ 
+SELECT NOW();
